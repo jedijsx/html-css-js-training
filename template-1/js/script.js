abf7caacce4fc98476dfbe91a13200a32563ddbf
Dmytro Puchkov <puchkov.dev@gmail.com>
2023-12-20 22:08:27 +0200

    update hero section

--- template-1/js/script.js
+++ template-1/js/script.js
@@ -1,10 +1,13 @@
 const header = document.querySelector('.header');
+const headerInner = document.querySelector('.header__inner');
 
 function checkScroll() {
   if (window.scrollY > 80) {
     header.style.backgroundColor = 'var(--color-bg-dark)';
+    headerInner.style.minHeight = '64px';
   } else {
     header.style.backgroundColor = '';
+    headerInner.style.minHeight = '96px';
   }
 }
 
